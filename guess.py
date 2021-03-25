51e5402a0bece09e6d83b4e71a4f5fcdaa1ed6ba
Saphira130 <alina.albin@web.de>
2021-03-25 20:30:28 +0100

    Update guess.py
    Anfangswert für guessedNumber vermieden

--- guess.py
+++ guess.py
@@ -1,17 +1,18 @@
 import random
 
 myNumber = random.randrange(0, 100, 1)
-guessedNumber = -1
 
 print("Ich habe mir eine Zahl von 0 bis 100 überlegt. Versuche sie zu erraten!")
 
-while guessedNumber != myNumber:
+while True:
     try:
         guessedNumber = int(input())
         if guessedNumber < myNumber:
             print("Meine Zahl ist größer.")
         if guessedNumber > myNumber:
             print("Meine Zahl ist kleiner.")
+        if guessedNumber == myNumber:
+            break
     except:
         print("Die Eingabe muss eine ganze Zahl sein.")
 
